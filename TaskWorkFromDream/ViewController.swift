f5a3a7d0f128faeb9ce71d93690c0a65a62f7d60
katsu <katsu.honcho@trifort.jp>
2020-01-29 13:30:14 +0900

    プロジェクト初期化する

---
+++ TaskWorkFromDream/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaskWorkFromDream
+//
+//  Created by katsu.honcho on 2020/01/29.
+//  Copyright © 2020 katsu.honcho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
